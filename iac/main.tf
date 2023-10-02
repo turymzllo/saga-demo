b3a38d8d454f2792b3262a7622e6b20d72ab1e85
Arturo Lara <turymzllo@gmail.com>
2023-10-02 00:12:28 -0300

    Fix auth rule id

--- iac/main.tf
+++ iac/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_eventhub_namespace_authorization_rule" "sagalogic-namespace-au
   name                = "${var.prefix}-nsauth-rule"
   namespace_name      = azurerm_eventhub_namespace.sagalogic-namespace.name
   resource_group_name = azurerm_resource_group.sagalogic-resource-group.name
-  id                  = azurerm_eventhub_namespace.sagalogic-namespace.id
+  id                  = 1
 
   listen = true
   send   = true
