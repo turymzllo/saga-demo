5b21b899a0ad777349a3e49b2f51a623aa91777b
Arturo Lara <turymzllo@gmail.com>
2023-10-02 00:17:52 -0300

    remove id under auth rule

--- iac/main.tf
+++ iac/main.tf
@@ -23,7 +23,6 @@ resource "azurerm_eventhub_namespace_authorization_rule" "sagalogic-namespace-au
   name                = "${var.prefix}-nsauth-rule"
   namespace_name      = azurerm_eventhub_namespace.sagalogic-namespace.name
   resource_group_name = azurerm_resource_group.sagalogic-resource-group.name
-  id                  = 1
 
   listen = true
   send   = true
