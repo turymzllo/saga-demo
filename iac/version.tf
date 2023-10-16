15124ac388b0b2c61d7f02f5d73c5dede30b9a5d
turymzllo <turymzllo@gmail.com>
2023-10-16 08:28:47 -0300

    Fix terraform provider two

--- iac/version.tf
+++ iac/version.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 0.12"
-  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "~>1.0"
     }
   }
 }
