e16bf3e7297fb7db8a391456d0a70ef1fa5f01b0
turymzllo <turymzllo@gmail.com>
2023-10-16 08:22:19 -0300

    Fix terraform provider

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,12 +1,8 @@
 provider "azurerm" {
-  version         = "= 2.14.0"
   subscription_id = var.sub
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
-  features {}
-}
 
-provider "random" {
-  version = "~> 2.2"
+  features {}
 }
